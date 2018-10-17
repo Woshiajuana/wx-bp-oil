8ead2aa04fb0738572090853367704b85f956c53
Woshiajuana <zhigang.chen@owulia.com>
2018-10-17 13:26:01 +0800

    更新

--- src/config/router.config.js
+++ src/config/router.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-    "home_index": "/pages/home/index"
+    "bill_index": "/pages/bill/index",
+    "home_index": "/pages/home/index",
+    "login_index": "/pages/login/index",
+    "mine_index": "/pages/mine/index"
 }
